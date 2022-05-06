8f33603b188e55d6cb15567e418e6edbedd1d78f
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-06 00:17:24 -0500

    Incomplete first proposal of solution
    This proposal doesn't use check_funds method in withdraw and tranfer methods neither create_spend_chart is completed

--- Budget-App/budget.py
+++ Budget-App/budget.py
@@ -1,7 +1,48 @@
 class Category:
 
+	def __init__(self, Name):
+		self.name = Name
+		self.ledger = []
 
+	def deposit(self, amount, description):
+		if description = False:
+			description = ""
+		self.ledger.append({"amount": amount, "description": description})
 
+	def withdraw(self, amount, description):
+		if description = False:
+			description = ""
+		total_amount = 0
+		for dictionary in self.ledger:
+			total_amount += dictionary["amount"]
+		if total_amount >= amount:
+			self.ledger.append({"amount": -amount, "description": description})
+			return True
+		else:
+			return False
+
+	def get_balance(self):
+		total_amount = 0
+		for dictionary in self.ledger:
+			total_amount += dictionary["amount"]
+		return total_amount
+
+	def transfer(self, amount, category_object):
+		total_amount = 0
+		for dictionary in self.ledger:
+			total_amount += dictionary["amount"]
+		if total_amount >= amount:
+			self.withdraw(amount, "Transfer to " + category_object.name)
+			category_object.deposit(amount, "Transfer from " + self.name)
+			return True
+		else:
+			return False
+
+	def check_funds(self, amount):
+		if amount > self.get_balance():
+			return False
+		else:
+			return True
 
 
 def create_spend_chart(categories):
