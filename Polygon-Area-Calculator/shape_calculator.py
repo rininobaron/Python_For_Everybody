3fc25da218a807f80131ca8b90825d5cb2b615b5
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-14 08:41:45 -0500

    Fix IdentationError
    Traceback (most recent call last):
      File "main.py", line 2, in <module>
        import shape_calculator
      File "/media/ricardo/FE242A58242A13E5/Users/LapTop/Desktop/Python_For_Everybody/Polygon-Area-Calculator/shape_calculator.py", line 7
        def set_width(self, width):
                                  ^
    IndentationError: unindent does not match any outer indentation level

--- Polygon-Area-Calculator/shape_calculator.py
+++ Polygon-Area-Calculator/shape_calculator.py
@@ -4,16 +4,16 @@ class Rectangle:
 		self.width = width
 		self.height = height
 
-    def set_width(self, width):
-    	self.width =  width
+	def set_width(self, width):
+		self.width = width
 
-    def set_height(self, height):
-    	self.height =  height
+	def set_height(self, height):
+		self.height = height
 
-    def get_area(self):
-    	return (self.width * self.height)
+	def get_area(self):
+		return (self.width * self.height)
 
-    def get_perimeter(self)::
+	def get_perimeter(self)::
     	return (2 * self.width + 2 * self.height)
     
     def get_diagonal(self)::
