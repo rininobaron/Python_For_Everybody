173e0422aeb64aa5dcc87cea92f1a361bd9b4dd7
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-14 08:47:42 -0500

    Fix SyntaxError
    Traceback (most recent call last):
      File "main.py", line 2, in <module>
        import shape_calculator
      File "/media/ricardo/FE242A58242A13E5/Users/LapTop/Desktop/Python_For_Everybody/Polygon-Area-Calculator/shape_calculator.py", line 16 (AND 19)
        def get_perimeter(self)::
                                ^
    SyntaxError: invalid syntax

--- Polygon-Area-Calculator/shape_calculator.py
+++ Polygon-Area-Calculator/shape_calculator.py
@@ -13,10 +13,10 @@ class Rectangle:
 	def get_area(self):
 		return (self.width * self.height)
 
-	def get_perimeter(self)::
+	def get_perimeter(self):
     	return (2 * self.width + 2 * self.height)
     
-    def get_diagonal(self)::
+    def get_diagonal(self):
     	return ((self.width ** 2 + self.height ** 2) ** .5)
     
     def get_picture(self):
