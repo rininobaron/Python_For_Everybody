f66cc624f3f7b82cdf6a47faa356e75db59b4a3e
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-15 00:11:49 -0500

    Fix IndentationError
    Traceback (most recent call last):
      File "main.py", line 2, in <module>
        import shape_calculator
      File "/media/ricardo/FE242A58242A13E5/Users/LapTop/Desktop/Python_For_Everybody/Polygon-Area-Calculator/shape_calculator.py", line 36
        def get_amount_inside(self, shape_objet):
                                                ^
    IndentationError: unindent does not match any outer indentation level

--- Polygon-Area-Calculator/shape_calculator.py
+++ Polygon-Area-Calculator/shape_calculator.py
@@ -33,29 +33,30 @@ class Rectangle:
 			picture = picture + '\n'
 		return picture
 
-    def get_amount_inside(self, shape_objet):
-    	'''OBJECT PARAMETERS'''
-    	#area_objet = shape_objet.get_area()
-    	perimeter_objet = shape_objet.get_perimeter()
-    	diagonal_objet = shape_objet.get_diagonal()
-    	width_object = shape_objet.width()
-    	height_object = shape_objet.height()
-    	'''SELF PARAMETERS'''
-    	#area = self.get_area()
-    	perimeter = self.get_perimeter()
-    	diagonal = self.get_diagonal()
-    	width = self.width()
-    	height = self.height()
-    	width = self.width()
-    	'''SOLUTION CASES'''
-    	if (height == height_object) and (width == width_object)::
-    		return 1
-    	elif (height > height_object) and (width > width_object):
-    		y_number = int(height/height_object)
-    		x_number = int(width/width_object)
-    		return x_number*y_number 
-    	else:
-    		return 0
+	def get_amount_inside(self, shape_objet):
+		'''OBJECT PARAMETERS'''
+		#area_objet = shape_objet.get_area()
+		perimeter_objet = shape_objet.get_perimeter()
+		diagonal_objet = shape_objet.get_diagonal()
+		width_object = shape_objet.width()
+		height_object = shape_objet.height()
+		'''SELF PARAMETERS'''
+		#area = self.get_area()
+		perimeter = self.get_perimeter()
+		diagonal = self.get_diagonal()
+		width = self.width()
+		height = self.height()
+		width = self.width()
+		'''SOLUTION CASES'''
+		if (height == height_object) and (width == width_object)::
+			return 1
+		elif (height > height_object) and (width > width_object):
+			y_number = int(height/height_object)
+			x_number = int(width/width_object)
+			return x_number*y_number
+		else:
+			return 0
+
 
 
 class Square(Rectangle):
