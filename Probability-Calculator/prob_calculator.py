d804945076f3da0afbca28664819123aa0eecbcf
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-16 07:19:03 -0500

    Try with my second code

--- Probability-Calculator/prob_calculator.py
+++ Probability-Calculator/prob_calculator.py
@@ -8,19 +8,40 @@ class Hat:
 		if not arguments:
 			self.balls = 1 # default
 			self.contents = ['default']
+			self.values = [1]
 		else:
 			contents = []
 			balls = 0
-			list(arguments.items())
-			for (key, value) in arguments.items():
-				contents.append(key)
-				values.append(value)
-				balls += value
+			items = list(arguments.items())
+			keys = list(arguments.keys())
+			values = list(arguments.values())
+			#print(contents)
+			counter = 0
+			for (key, value) in items:
+				for i in range(value):
+					contents.append(key)
+			print(contents)
+			# 	#if counter == 0:
+			# 		#ksjssk
+			# 	counter += 1
+			# 	values.append(value)
+			# 	balls += value
 			self.contents = contents
-			self.balls = balls
-
+			self.balls = sum(values)
+			self.values = values
 		print(self.balls)
 		print(self.contents)
 			
-	def draw(self, balls):
-		self.balls = balls
+	def draw(self, balls_draw=None):
+		if not balls_draw:
+			return 'No balls to draw'
+		elif balls_draw > self.balls:
+			return 'balls to draw are more than balls inside the hat'
+		print(balls_draw)
+		print(random.random()*(balls_draw/self.balls))
+
+
+
+# Random Case for Testing
+hat = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+print(hat.draw(8))
