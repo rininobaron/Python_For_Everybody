b8ffc1954c30e80f44ee37fa69d55b614e3703cf
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-22 02:20:13 -0500

    Delete many prints statements and random case

--- Probability-Calculator/prob_calculator.py
+++ Probability-Calculator/prob_calculator.py
@@ -21,17 +21,9 @@ class Hat:
 			for (key, value) in items:
 				for i in range(value):
 					contents.append(key)
-			print(contents)
-				# #if counter == 0:
-				# 	#ksjssk
-				# counter += 1
-				# values.append(value)
-				# balls += value
 			self.contents = contents
 			self.balls = sum(values)
 			self.values = values
-		print(self.balls)
-		print(self.contents)
 
 	def draw(self, balls_draw=None):
 		balls = self.balls
@@ -52,27 +44,17 @@ class Hat:
 			arguments_random[key] = self.arguments[key] 
 		keys_random = arguments_random.keys()
 		values_random = arguments_random.values()
-		print('value', 'random_num', 'int(value*random_num)')
 		for value in values_random:
 			if value == 0:
 				values_random_temp.append(value)
-				print(value, random_num, int(value))
 				continue
 			random_num = random.random()
 			balls -= int(value*random_num)
 			if balls < balls_final:
 				balls += int(value*random_num)
 				values_random_temp.append(value)
-				print(value, random_num, int(0))
 			else:
 				values_random_temp.append(value - int(value*random_num))
-				print(value, random_num, int(value*random_num))
-			# elif balls == balls_final:
-			# 	values_random_temp.append(value - int(value*random_num))
-			# 	print(value, random_num, int(0))
-			# else:
-			# 	values_random_temp.append(value - int(value*random_num))
-			# 	print(value, random_num, int(value*random_num))
 		'''WORKING'''
 		if not len(values_random_temp) == len(values):
 			return print('ERROR1')
@@ -91,39 +73,23 @@ class Hat:
 					if balls == balls_final:
 						break
 					if values_random_temp[i] == 0:
-						print('balls_final: ', str(balls_final))
-						print('balls: ', str(balls))
 						continue
 					if values_random_temp[i] > 0:
 						balls -= 1
 						values_random_temp[i] -= 1
-						print('balls_final: ', str(balls_final))
-						print('balls: ', str(balls))
 		'''REARRANGE values_temp ACCORDING TO ORIGINAL values'''
 		for key in keys:
 			for (key_r, value_r) in zip(keys_random, values_random_temp):
 				if key == key_r:
 					values_temp.append(value_r)
-		print('random probability (random number 0 to 1): ' + str(random.random()))
-		print('balls_original: ' + str(self.balls))
-		print('balls_final: ' + str(balls))
-		print('values: ' + str(self.values))
-		print('values_temp: ' + str(values_temp))
 		contents = []
-		print()
-		print()
-		print(self.contents)
 		for value, value_temp, key in zip(values, values_temp, self.keys):
-			print(value, value_temp, key)
 			if value_temp == 0:
 				continue
 			string_temp = value_temp*(key+',')
-			print(string_temp[:-1])
 			string_split = string_temp[:-1].split(',')
 			for tag in string_split:
 				contents.append(tag)
-			print(value_temp, ' new ', key,' tags was appended successfully!')
-		print('\n' + 'contents: ' + '\n' + str(contents))
 		'''UPDATING ATTRIBUTES'''
 		arguments = {}
 		for key, value in zip(keys, values_temp):
@@ -136,8 +102,6 @@ class Hat:
 		return self.contents
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-	for i in expected_balls:
-		print(i, expected_balls[i])
 	N = num_experiments
 	M = 0
 	for i in range(N):
@@ -155,15 +119,3 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 		if len(colors) == len(list(expected_balls.keys())):
 			M += 1
 	return M/N
-
-
-# Random Case for Testing
-hat = Hat(red=20, orange=10, black=1, blue=0, pink=2, striped=9)
-print('balls: ' + str(hat.balls))
-print(hat.draw())
-print()
-print()
-# print(experiment(hat=hat, 
-#                   expected_balls={"red":2,"orange":1}, 
-#                   num_balls_drawn=5, 
-#                   num_experiments=8))
