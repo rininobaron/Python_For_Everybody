aa1654410195c4357c194458c48bf5664fcce612
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-05-23 00:35:34 -0500

    Fix many issues
    The method draw was edited many times in order to fit to the requirements

--- Probability-Calculator/prob_calculator.py
+++ Probability-Calculator/prob_calculator.py
@@ -16,8 +16,6 @@ class Hat:
 			items = list(arguments.items())
 			self.keys = list(arguments.keys()) # Add like attribute in order to use method 'draw'
 			values = list(arguments.values())
-			#print(contents)
-			counter = 0
 			for (key, value) in items:
 				for i in range(value):
 					contents.append(key)
@@ -33,86 +31,43 @@ class Hat:
 			return 'No balls to draw'
 		elif balls_draw > balls:
 			return self.contents
-		balls_final = self.balls - balls_draw
-		#prob = balls_draw/balls
-		values_temp = []
-		#random values abd tags
-		values_random_temp = []
-		arguments_random_list = sorted(self.arguments)
-		arguments_random = {}
-		for key in arguments_random_list:
-			arguments_random[key] = self.arguments [key] 
-		keys_random = arguments_random.keys()
-		values_random = arguments_random.values()
-		for value in values_random:
-			if value == 0:
-				values_random_temp.append(value)
-				continue
-			random_num = random.random()
-			balls -= int(value*random_num)
-			if balls < balls_final:
-				balls += int(value*random_num)
-				values_random_temp.append(value)
-			else:
-				values_random_temp.append(value - int(value*random_num))
-		'''WORKING'''
-		if not len(values_random_temp) == len(values):
-			return print('ERROR1')
-		if sum(values_random_temp) == balls:
-			pass
-		else:
-			print('sum(values_random_temp): ', sum(values_random_temp))
-			print('balls: ', balls)
-			return print("ERROR")
-		'''WORKING'''
-		if balls > balls_final:
-			while balls > balls_final:
-				if balls == balls_final:
-					break
-				for i in range(len(values_random_temp)):
-					if balls == balls_final:
-						break
-					if values_random_temp[i] == 0:
-						continue
-					if values_random_temp[i] > 0:
-						balls -= 1
-						values_random_temp[i] -= 1
-		'''REARRANGE values_temp ACCORDING TO ORIGINAL values'''
-		for key in keys:
-			for (key_r, value_r) in zip(keys_random, values_random_temp):
-				if key == key_r:
-					values_temp.append(value_r)
-		contents = []
-		for value, value_temp, key in zip(values, values_temp, self.keys):
-			if value_temp == 0:
-				continue
-			string_temp = value_temp*(key+',')
-			string_split = string_temp[:-1].split(',')
-			for tag in string_split:
-				contents.append(tag)
-		'''UPDATING ATTRIBUTES'''
-		arguments = {}
-		for key, value in zip(keys, values_temp):
-			arguments[key] = value
 		'''
 		FIXING LOGICAL PROBLEM
 
-		The method returns the contents list, 
-		but the method must return only 
-		a list with the balls draw to the hat
+		The draw method code is incorrect according to requirements
 		'''
-		values_draw = [(i - temp) for i, temp in zip(self.values, values_temp)]
+
+		arguments_temp = self.arguments
+		tuples = list(self.arguments.items())
+		tuples_temp = copy.copy(tuples)
+		counter = 0
+		while counter < balls_draw:
+			temp = random.choice(tuples_temp)
+			key = temp[0]
+			if arguments_temp[key] > 0:
+				arguments_temp -= 1
+				counter += 1
+			elif sum(arguments_temp.values()) == 0:
+				break
+			else:
+				continue
 		contents_draw = []
-		for value_draw, key in zip(values_draw, self.keys):
+		for (value_final, key) in list(arguments_temp.items()):
+			value_draw = self.arguments[key] - value_final
 			if value_draw == 0:
 				continue
 			string_temp = value_draw*(key+',')
 			string_split = string_temp[:-1].split(',')
-			contents_draw = [tag for tag in string_split]
-		self.arguments = arguments
-		self.balls = balls
-		self.keys = keys
-		self.values = values_temp
+			for tag in string_split:
+				contents_draw.append(tag)
+		self.arguments = arguments_temp
+		self.balls = sum(arguments_temp.values())
+		self.keys = list(arguments_temp.keys())
+		self.values = list(arguments_temp.values())
+		contents = []
+		for (key, value) in self.arguments:
+				for i in range(value):
+					contents.append(key)
 		self.contents = contents
 		self.contents_draw = contents_draw
 		return self.contents_draw
@@ -135,3 +90,15 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 		if len(colors) == len(list(expected_balls.keys())):
 			M += 1
 	return M/N
+
+
+# TESTING EXAMPLE
+random.seed(95)
+hat = Hat(blue=4, red=2, green=6)
+probability = experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+print("Probability:", probability)
